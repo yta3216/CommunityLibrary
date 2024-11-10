692e7fa49237e9b8ad93bce6ca3ab6477b7b1389
Kiichiro-suganuma0209 <kiichiro.suganuma0209@gmail.com>
2024-11-10 02:58:38 -0800

    backend deploy

---
+++ my-medusa-store/storefront/src/modules/home/components/hero/index.tsx
@@ -0,0 +1,37 @@
+import { Github } from "@medusajs/icons"
+import { Button, Heading } from "@medusajs/ui"
+
+const Hero = () => {
+  return (
+    <div className="h-[100vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    {/* <div className="hidden"> */}
+
+      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+        <span>
+          <Heading
+            level="h1"
+            className="text-3xl leading-10 text-ui-fg-base font-normal"
+          >
+            Cover picture Here
+          </Heading>
+          <Heading
+            level="h2"
+            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+          >
+          </Heading>
+        </span>
+        {/* <a
+          href="https://github.com/medusajs/nextjs-starter-medusa"
+          target="_blank"
+        >
+          <Button variant="secondary">
+            View on GitHub
+            <Github />
+          </Button>
+        </a> */}
+      </div>
+    </div>
+  )
+}
+
+export default Hero
