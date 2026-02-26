4028a7a954247169ea9aa48b665356b4d4bb66b0
Samuell Bueno <samuellggbueno@gmail.com>
2026-02-26 13:53:46 -0800

    made multiple edits to the database structure now it reinforces some behaviours that will be needed for our website.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,21 +1,44 @@
 const mongoose = require("mongoose");
 
+const ALLOWED_ROLES = ["admin", "user"];
+
 const userSchema = new mongoose.Schema({
-  name: {
+  username: {
     type: String,
     required: true,
     unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    trim: true,
+    lowercase: true,
   },
   password: {
     type: String,
     required: true,
+    hide: true,
   },
-
-}, { timestamps: true });
+  numberOfBooks: {
+    type: Number,
+    required: true,
+    default: 0,
+    min: 0,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ALLOWED_ROLES,
+    default: "user",
+  },
+});
 
 module.exports = mongoose.model("User", userSchema);
--- backend/server.js
+++ backend/server.js
@@ -10,25 +10,33 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(async () => {
     console.log("MongoDB connected");
 
     // creating test data
     const user = await User.create({
+      username: "kiichiro",
       name: "Kiichiro",
       email: "kiichiro@test.com",
+      password: "test1234",
+      numberOfBooks: 1,
+      role: "user",
     });
 
     const book = await Book.create({
+      isbn: "9780000000000",
       title: "Test Book",
       author: "Tester",
       owner: user._id,
+      holder: user._id,
+      status: "with_owner",
     });
 
     console.log("User & Book created");
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 app.listen(5000, () => {
   console.log("Server running");
