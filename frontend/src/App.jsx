990d7980436120b4e3fa81c4d7f79d897e920c47
su23-t <sthwe@student.ubc.ca>
2026-03-09 00:13:13 -0700

    Modified routes with seperating visibilitities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/UnregisteredHome";
-import { Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import UnregisteredHome from "./pages/UnregisteredHome";
+import RegisteredHome from "./pages/RegisteredHome";
 import Login from "./components/Form/Login";
 import Register from "./components/Form/Register";
 import AdminHome from "./components/adminHome";
 import AdminBooks from "./components/adminBooks";
 import AdminUsers from "./components/adminUsers";
+import BookDetail from "./pages/BookDetail";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/*Public pages - unregistered users*/}
+        <Route path="/" element={<UnregisteredHome />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        {/*Registered users pages*/}
+        <Route path="/home" element={<RegisteredHome />} />
+        <Route path="/book" element={<BookDetail />} />
+
+        {/*Admin Pages*/}
         <Route path="/admin/home" element={<AdminHome />} />
         <Route path="/admin/books" element={<AdminBooks />} />
         <Route path="/admin/users" element={<AdminUsers />} />
+
+        {/*Redirect for undefined routes*/}
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
